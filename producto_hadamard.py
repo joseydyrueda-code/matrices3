b91cdfe10080b8c554a88faa8cf58d990ae5a996
joseydyrueda-code <joseydyrueda@gmail.com>
2026-04-27 08:27:44 -0500

    Add files via upload

---
+++ producto_hadamard.py
@@ -0,0 +1,14 @@
+def producto_hadamard(A, B):
+  filas = len(A)
+  columnas = len(A[0])
+  if filas != len(B) or columnas != len(B[0]):
+   return "Error: las matrices deben tener las mismas dimensiones"
+
+  else:
+    resultado = []
+    for i in range(filas):
+      fila = []
+      for j in range(columnas):
+        fila.append(A[i][j] * B[i][j])
+      resultado.append(fila)
+  return resultado
